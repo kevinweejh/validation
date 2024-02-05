2ea149fc55d04ceba6fd02978e95d6131bf067d2
Wee Jian Hui Kevin <hello@codebykevin.dev>
2024-02-05 17:33:33 +0800

    feat: create basic layout and query selections

--- src/index.js
+++ src/index.js
@@ -1,42 +1,20 @@
 import './style.css';
-import InitialPageLoad from './initialPageLoad.js';
-import TabBar from './tabBar.js';
-import TabContent from './tabContent.js';
-import HomeTab from './homeTab.js';
-import MenuTab from './menuTab.js';
-import ContactTab from './contactTab.js';
 
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
 }
 
-InitialPageLoad();
+const validationBtn = document.querySelector("#validate");
+const modal = document.querySelector("#validation");
 
-TabBar();
+validationBtn.addEventListener("click", () => {
+    modal.showModal();
+})
 
-const { tabContentDiv } = TabContent();
-HomeTab();
+const emailInput = document.querySelector("#email");
+const countryInput = document.querySelector("#country");
+const zipInput = document.querySelector("#zip");
+const passwordInput = document.querySelector("#password");
+const passwordConfirmationInput = document.querySelector("#passwordConfirmation");
 
-const tabSwitching = () => {
-    const tabBtns = document.querySelectorAll('[id$="Btn"]');
-    const btnMapping = {
-        'homeTabBtn': HomeTab,
-        'menuTabBtn': MenuTab,
-        'contactTabBtn': ContactTab,
-    }
 
-    tabBtns.forEach((btn) => {
-        btn.addEventListener('click', () => {
-            tabContentDiv.innerHTML = '';
-
-            const moduleFunction = btnMapping[btn.id];
-            if (moduleFunction) {
-                moduleFunction();
-            } else {
-                console.error('No matching module function found for button:' , btn.id);
-            }
-        })
-    })
-}
-
-tabSwitching();
